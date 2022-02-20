75da1501bc22a19fd4b6dd67752e0d0491860c01
Nicholas Place <nplace.np@gmail.com>
2022-02-20 14:07:31 -0800

    Adding hellocolor

---
+++ hellocolor/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  // sets up the size of display + makes cursor invisible
+  createCanvas(720, 720);
+  noCursor();
+
+  colorMode(HSB, 360, 100, 100);
+  rectMode(CENTER);
+  noStroke();
+}
+
+function draw() {
+  background(mouseY / 2, 100, 100);
+
+  fill(360 - mouseY / 2, 100, 100);
+  rect(360, 360, mouseX + 1, mouseX + 1);
+}
